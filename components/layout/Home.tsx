6da046ab0568dc243e37af3de2e4a3ee98e04ee5
devhiko <aydin96tr@gmail.com>
2022-08-01 23:14:30 +0300

    deleted bulma

--- components/layout/Home.tsx
+++ components/layout/Home.tsx
@@ -2,26 +2,9 @@
 const Home = () => {
 
   return (
-    <>
-      <div>
-        <div className="card" style={{ margin: '1rem' }}>
-          <div className="card-content">
-
-            <div className="media">
-              <div className="media-content">
-                <p className="title is-4">Not Başlığı</p>
-              </div>
-            </div>
-
-            <div className="content">
-              Not içeriği <br />
-              <span>20:03</span>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+      Home
+    </div>
   )
 }
 
