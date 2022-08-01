8230e9a09a294cbdd8b438159280924d6497eb22
devhiko <aydin96tr@gmail.com>
2022-08-01 20:06:07 +0300

    added bulma

--- components/layout/Home.tsx
+++ components/layout/Home.tsx
@@ -2,7 +2,26 @@
 const Home = () => {
 
   return (
-    <div>Home</div>
+    <>
+      <div>
+        <div className="card" style={{ margin: '1rem' }}>
+          <div className="card-content">
+
+            <div className="media">
+              <div className="media-content">
+                <p className="title is-4">Not Başlığı</p>
+              </div>
+            </div>
+
+            <div className="content">
+              Not içeriği <br />
+              <span>20:03</span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout/Layout'
+// import '../styles/globals.css'
+import 'bulma/css/bulma.min.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
