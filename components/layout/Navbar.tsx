bf2f9d6c2eee9428545fe44e0b5e3aeb3328c7b6
devhiko <aydin96tr@gmail.com>
2022-08-02 00:52:13 +0300

    added material and edited content

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,9 +1,41 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from '@mui/material';
 
 const Navbar = () => {
   return (
-    <div style={{ background: '#f3f3f3' }}>
-      Navbar
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color='secondary'>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <MenuItem>
+            <Link href='/' style={{ color: '#fff' }}>
+              <Typography textAlign="center">Notlar</Typography>
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Typography textAlign="center">Görevler</Typography>
+          </MenuItem>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout/Layout'
-// import '../styles/globals.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
-    </Layout>
+    </Layout >
   )
 }
 
