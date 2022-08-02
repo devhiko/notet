fded3aceadd627e58c960ea114510f9166cbc033
devhiko <aydin96tr@gmail.com>
2022-08-02 21:27:34 +0300

    added note card

--- components/layout/Home.tsx
+++ components/layout/Home.tsx
@@ -1,17 +1,25 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Fab from '@mui/material/Fab'
 
 const Home = () => {
 
   return (
     <div>
       Home
-
+      <Card sx={{ margin: '1rem auto' }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Not Başlığı
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Not İçeriği
+          </Typography>
+          <Typography sx={{ m: '.5rem auto' }} variant="subtitle2" color="text.secondary">
+            16:12
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
